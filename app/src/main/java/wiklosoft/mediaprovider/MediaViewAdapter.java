81630fa6061d23ac7db214d809c35fa1624a3efb
Pawel Wiklowski <pawel.wiklowski@gmail.com>
2015-11-12 22:23:32 +0100

    Show icons when exists

--- app/src/main/java/wiklosoft/mediaprovider/MediaViewAdapter.java
+++ app/src/main/java/wiklosoft/mediaprovider/MediaViewAdapter.java
@@ -1,7 +1,11 @@
 package wiklosoft.mediaprovider;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.browse.MediaBrowser;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +15,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import java.net.URL;
 import java.util.List;
 
 import wiklosoft.mediaprovider.playlists.Playlist;
@@ -37,7 +42,7 @@ public class MediaViewAdapter extends ArrayAdapter<MediaBrowser.MediaItem> {
         final MediaBrowser.MediaItem item = getItem(position);
 
         TextView title = (TextView) rowView.findViewById(R.id.title);
-        ImageView typeIcon = (ImageView) rowView.findViewById(R.id.item_type);
+        final ImageView typeIcon = (ImageView) rowView.findViewById(R.id.item_type);
         final ToggleButton star = (ToggleButton) rowView.findViewById(R.id.star);
 
         if(item.isBrowsable()){
@@ -64,6 +69,29 @@ public class MediaViewAdapter extends ArrayAdapter<MediaBrowser.MediaItem> {
             } else {
                 star.setBackgroundResource(R.mipmap.star_off);
             }
+        }
+
+        if (item.getDescription().getIconUri() != null){
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        Uri u = item.getDescription().getIconUri();
+                        final Bitmap b = BitmapFactory.decodeStream(new URL(u.toString()).openConnection().getInputStream());
+                        ((Activity)getContext()).runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                typeIcon.setImageBitmap(b);
+                            }
+                        });
+
+                    } catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+            t.start();
+
         }
 
 
